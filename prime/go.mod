ccee2148e6d654ec3bf131648e73f60abf7e9ff4
Shaunak Pandit <41136147+shaunakpandit@users.noreply.github.com>
2025-08-09 14:21:38 -0400

    recreate prime number tool

---
+++ prime/go.mod
@@ -0,0 +1,3 @@
+module jssp.io/prime
+
+go 1.24.4
---
+++ prime/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	i := os.Args[1]
+	num, err := strconv.Atoi(i)
+
+	if err != nil {
+		fmt.Println("Expecting a number. Value given: ", i)
+	}
+
+	abs(&num)
+
+	fmt.Println(num, " is prime: ", isPrime(num, num-1))
+}
+
+func abs(n *int) int {
+	if *n > 0 {
+		return *n
+	} else if *n == 0 {
+		return 0
+	} else {
+		return -1 * *n
+	}
+}
+
+func isPrime(num, div int) bool {
+	if num == 0 || num == 1 {
+		return false
+	}
+	if div == 1 {
+		return true
+	}
+	if num%div == 0 {
+		return false
+	}
+	return isPrime(num, div-1)
+}
