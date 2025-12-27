0b223bc16d24a514876323a009d7bfca4b604cbc
Shaunak Pandit <41136147+shaunakpandit@users.noreply.github.com>
2025-12-27 13:45:32 -0600

    pass in size of tree

--- tree/main.go
+++ tree/main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-	baseSize := 15
-	// fmt.Println("*")
+	i := os.Args[1]
+	num, err := strconv.Atoi(i)
+
+	if err != nil {
+		message := "Expecting a number. Value given: " + i
+		panic(message)
+	}
+	if num%2 == 0 {
+		message := "Expecting an odd number. Value given: " + i
+		panic(message)
+	}
+
+	baseSize := num
 	for i := 0; i <= baseSize; i += 2 {
 		layerSpaces := (baseSize - i) / 2
 		layerStars := baseSize - layerSpaces*2
