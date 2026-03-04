ff3f8fa37c60e45a1ce6b586b3d87f1457485118
shaunakpandit <shaunakdpandit@gmail.com>
2026-03-03 23:30:30 -0500

    bubblesort created
    Signed-off-by: shaunakpandit <shaunakdpandit@gmail.com>

--- sortingAlgos/bubblesort/bubblesort.go
+++ sortingAlgos/bubblesort/bubblesort.go
@@ -1,10 +1,14 @@
 package bubblesort
 
-import "fmt"
-
 func BubbleSort(unsorted []int) []int {
-	for i, v := range unsorted {
-		fmt.Println("index: ", i, " value: ", v)
+	for j := 0; j < len(unsorted)-1; j++ {
+		for i := 0; i < len(unsorted)-1-j; i++ {
+			if unsorted[i] > unsorted[i+1] {
+				temp := unsorted[i+1]
+				unsorted[i+1] = unsorted[i]
+				unsorted[i] = temp
+			}
+		}
 	}
 	return unsorted
 }
